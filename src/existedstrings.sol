008154d5f00939a2422fddac3f27345f4f180a53
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-10-28 00:30:46 +0400

    String memory self

---
+++ src/existedstrings.sol
@@ -0,0 +1,8 @@
+ function toSlice(string memory self) internal pure returns (slice memory) {
+        uint ptr;
+        assembly {
+            ptr := add(self, 0x20)
+        }
+        return slice(bytes(self).length, ptr);
+    }
+ 
